4fa8b7738571eb4018f2a63944382a3c4d5cf964
nur12islam <68411946+nur12islam@users.noreply.github.com>
2026-09-17 00:46:14 +0530

    android: add Room database

---
+++ android/app/src/main/java/com/darki/cloud/data/local/CloudDatabase.kt
@@ -0,0 +1,22 @@
+package com.darki.cloud.data.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [UserEntity::class, DeviceEntity::class, FolderEntity::class, FileEntity::class],
+    version = 1,
+    exportSchema = false,
+)
+abstract class CloudDatabase : RoomDatabase() {
+    abstract fun cloudDao(): CloudDao
+
+    companion object {
+        fun create(context: Context): CloudDatabase =
+            Room.databaseBuilder(context, CloudDatabase::class.java, "darki-cloud.db")
+                .fallbackToDestructiveMigration()
+                .build()
+    }
+}
